85be545b0059ff53f3f30397edd65feaf00d0bf7
Kavita Germain <kavitagermain@MacBook-Air-de-Kavita.local>
2016-11-10 17:56:56 +0100

    add folders and node architecture

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+//app.get('/', function(req, res) {
+//    res.send();
+// });
+
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.use('/img', express.static(__dirname + '/img'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/html/home.html');
+});
+
+http.listen(1337, function(){
+        console.log('listening on *:1337');
+});

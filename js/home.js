e1900d03d8ec6447dfd73bda982547a904d523a3
Kavita Germain <kavitagermain@MacBook-Air-de-Kavita.local>
2016-11-16 23:41:31 +0100

    finish last bloc, footer. Add menu hamburger and scroll to plugin

---
+++ js/home.js
@@ -0,0 +1,37 @@
+//Hamburger Menu
+
+(function() {
+
+  'use strict';
+
+  document.querySelector('.material-design-hamburger__icon').addEventListener(
+    'click',
+    function() {      
+      var child;
+      
+      document.body.classList.toggle('background--blur');
+      this.parentNode.nextElementSibling.classList.toggle('menu--on');
+
+      child = this.childNodes[1].classList;
+
+      if (child.contains('material-design-hamburger__icon--to-arrow')) {
+        child.remove('material-design-hamburger__icon--to-arrow');
+        child.add('material-design-hamburger__icon--from-arrow');
+      } else {
+        child.remove('material-design-hamburger__icon--from-arrow');
+        child.add('material-design-hamburger__icon--to-arrow');
+      }
+
+    });
+
+})();
+
+//Scroll To
+	$(document).ready(function() {
+		$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 1250; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+	});
